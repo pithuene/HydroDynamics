ab6b764c0c250d0ca75ef849e1fce31ddb620178
pithuene <pitsimonhuene@t-online.de>
2019-11-06 16:24:46 +0100

    Refactor approachGrab

--- arm.py
+++ arm.py
@@ -1,10 +1,15 @@
 from pybricks.ev3devices import (Motor)
 from pybricks.parameters import (Port, Stop)
 
+import utils
+
 motorArmMovement = Motor(Port.C)
 motorArmGrip = Motor(Port.D)
 
 torque = 80
+
+grabDistance = [70, 80]
+
 
 def up():
     motorArmMovement.run_until_stalled(100, Stop.HOLD, torque)
@@ -28,3 +33,24 @@ def grab():
     down()
     close()
     up()
+
+
+def approachGrab(us, nav):
+    while (True):
+        dist = utils.preciseDistance(us, 3)
+        if(dist > 150):
+            nav.driveDistance(100, 100)
+        else:
+            break
+
+    while (True):
+        dist = utils.preciseDistance(us, 10)
+        if(dist < grabDistance[0]):
+            # Back
+            nav.driveDistance(-25, 50)
+        elif (dist > grabDistance[1]):
+            # Forward
+            nav.driveDistance(25, 50)
+        else:
+            break
+    grab()
